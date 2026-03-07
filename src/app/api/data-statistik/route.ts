81875f84583221280440db1a654f8d6376992cd4
ArryPutra <arrykusumaputra04@gmail.com>
2026-03-07 16:24:10 +0800

    menambahkan key pada api data statistik

--- src/app/api/data-statistik/route.ts
+++ src/app/api/data-statistik/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(_request: NextRequest) {
     const dataStatistik = await prisma.dataStatistik.findMany({
         select: {
+            key: true,
             label: true,
             value: true
         }
